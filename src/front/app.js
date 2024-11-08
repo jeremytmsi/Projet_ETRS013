6a381909a61560b67fa5b0d2f9df1855e2dedb81
Jeremy Tomasi <contact@jeremytomasi.fr>
2024-11-08 14:17:03 +0100

    itineraire

--- src/front/app.js
+++ src/front/app.js
@@ -4,3 +4,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.Routing.control({
+    geocoder: L.Control.Geocoder.nominatim()
+}).addTo(map)
