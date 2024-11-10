19e4bb7625620e2793f9d41a377abf0712fd0097
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-11-10 16:06:50 +0100

    mise en forme HTML

--- src/front/index.js
+++ src/front/index.js
@@ -3,8 +3,10 @@ let app = express()
 let port = 80
 let fs = require("fs")
 const path = require("path")
+let cors = require("cors")
 
 app.use(express.static(__dirname))
+app.use(cors())
 
 app.get('/',(req,res) => {
     res.sendFile("index.html",{root: path.join(__dirname)})
