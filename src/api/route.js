8862f99fe77ec9bbbbc479be754621b3c5183b7a
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-11-11 20:12:31 +0100

    (api): env file support

--- src/api/route.js
+++ src/api/route.js
@@ -7,15 +7,17 @@ router.post("/", async (req,res) => {
     let start_coordinates = await get_coordinates_from_address(req.query.startPoint)
     let end_coordinates = await get_coordinates_from_address(req.query.endPoint)
 
+    let intermediateCoordinates = req.query.intermediateCoordinates
+
     let response = await fetch("https://api.openrouteservice.org/v2/directions/driving-car/geojson",{
         method: "POST",
         headers: {
             "Content-Type": "application/json; charset=UTF-8",
             "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
-            "Authorization": "5b3ce3597851110001cf62486808877b35464842b5f6a16ca8f2acf4"
+            "Authorization": process.env.OPENROUTESERVICE_API_KEY
         },
         body: JSON.stringify({
-            coordinates: [start_coordinates, end_coordinates]
+            coordinates: intermediateCoordinates == null ? [start_coordinates,end_coordinates] : [start_coordinates, intermediateCoordinates, end_coordinates]
         })
     })
 
