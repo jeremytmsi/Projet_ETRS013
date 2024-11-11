69e6920d2c1698fd5b6d5bfc694d09f129e7be7e
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-11-11 19:37:23 +0100

    get station details

--- src/api/stations.js
+++ src/api/stations.js
@@ -3,8 +3,8 @@ let router = express.Router()
 
 let {chargetrip_req} = require("./api_request")
 
-router.get("/around",(req,res) => {
-    chargetrip_req(`
+router.get("/around",async (req,res) => {
+    let response = await chargetrip_req(`
         query stationAround {
             stationAround(
                 filter: {
@@ -21,16 +21,43 @@ router.get("/around",(req,res) => {
             ) {
                 id
                 name
-                location {
-                    type
-                    coordinates
-                }    
             }
                 
         }
-    `).then(data => {
-        res.send(data)
-    }).catch(err => console.error(err))
+    `)
+    
+    res.send(response)
+})
+
+router.get("/details",async (req,res) => {
+    let response = await chargetrip_req(`
+        query station {
+            station(id: "${req.query.stationId}"){
+                id
+                name
+                address
+                city
+                postal_code
+                country
+                coordinates {
+                    latitude
+                    longitude
+                }
+                evses {
+                    evse_id
+                    connectors {
+                        id
+                        standard
+                        format
+                        power
+                        power_type
+                    }
+                }
+            }
+        }
+    `)
+    let details = response.station
+    res.send(details)
 })
 
 module.exports = router
