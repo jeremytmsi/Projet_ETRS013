c05ba013ba0eb4558fa0fdbbdf6dd04f3b560e28
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-11-11 20:22:41 +0100

    add marker at start and end

--- src/front/app.js
+++ src/front/app.js
@@ -166,6 +166,8 @@ document.getElementById('form-submit').addEventListener("click",async (e) => {
     let coordinates = route_data.features[0].geometry.coordinates
     let base_coordinates = route_data.features[0].geometry.coordinates
     drawRoute(layers.routeLayer,coordinates)
+    addMarker(layers.routeLayer,route_data.metadata.query.coordinates[0][1],route_data.metadata.query.coordinates[0][0],"Depart", icons.def)
+    addMarker(layers.routeLayer,route_data.metadata.query.coordinates[1][1],route_data.metadata.query.coordinates[1][0],"Arrivée", icons.def)
 
     for(let i=0; i < nbRecharges; i++){
         let circle = null
@@ -179,6 +181,7 @@ document.getElementById('form-submit').addEventListener("click",async (e) => {
         console.log(coordinatesSearchStation)
         let station_res = await fetch(`http://localhost:3000/api/stations/around?lon=${coordinatesSearchStation[1]}&lat=${coordinatesSearchStation[0]}`)
         let station_data = await station_res.json()
+        station_data = station_data.stationAround
         console.log(station_data)
         addMarker(layers.routeLayer,coordinatesSearchStation[1],coordinatesSearchStation[0],"Recherche station",icons.def)
 
